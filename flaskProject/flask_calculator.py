4ead524bc8c416f7c7258bf55083913468287122
Konrad Zemczak <zemczakk@gmail.com>
2024-11-13 21:32:27 +0100

    fix flask import

--- flaskProject/flask_calculator.py
+++ flaskProject/flask_calculator.py
@@ -1,8 +1,7 @@
 """Import necessary modules"""
-import flask
-from flask import Flask
-from flask import jsonify
-app=Flask(__name__)# name to specjalna zmienna, która się różni od tego w jaki sposób wykonam skrypt
+from flask import Flask, request, jsonify
+
+app=Flask(__name__)# name to specjalna zmienna, która się różni od tego w jaki sposób wykon
 
 
 @app.route('/') #jeśli ktoś odpyta serwer pod tą scieżką zrób to
@@ -12,9 +11,9 @@ def hello_world():  # put application's code here
 @app.route('/calculate')
 def calculate():
     """this function calculates using get function."""
-    op = flask.request.args.get('op', type=str)
-    arg1 = flask.request.args.get('arg1', type=float)
-    arg2 = flask.request.args.get('arg2', type=float)
+    op = request.args.get('op', type=str)
+    arg1 = request.args.get('arg1', type=float)
+    arg2 = request.args.get('arg2', type=float)
     if op is None or arg1 is None or arg2 is None:
         return jsonify({"error": "Brakujące parametry"}), 400
     if op == 'sum':
